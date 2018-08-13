e7ee3db0d2202edd35a26d7b2b5153629ad2aba7
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-08-13 01:21:00 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -3,5 +3,10 @@ def line(katz_deli)
   case
   when katz_deli.length == 0
     puts "The line is currently empty."
+  when katz_deli.length > 0
+    line_announce = "The line is currently: "
+    katz_deli.each do |name, index|
+      line_announce << "#{index}. #{name}"
+    end
   end
 end
