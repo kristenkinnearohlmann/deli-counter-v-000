605a9217462c215b4e8b2f46e817983a5377b109
Kristen Kinnear-Ohlmann <kristenkinnearohlmann@gmail.com>
2018-08-13 01:25:09 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -12,3 +12,9 @@ def line(katz_deli)
     puts line_announce
   end
 end
+
+def take_a_number(katz_deli, name)
+  if katz_deli.length == 0
+    puts "Welcome, #{name}. You are number 1 in line."
+  end
+end
